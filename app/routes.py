7ee6aaa5477ffd97fd343ab99a2e90c5f3b41924
tanej.buhin <tanej.buhin@student.um.si>
2025-05-13 19:02:16 +0200

    Added basic views for team's matches and also team's squad

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, Response
-from .utils import get_matches_from_api  # Uvoz funkcije iz utils.py
+from .utils import get_matches_from_api
+from .utils import get_team_matches
 import json
 
 main = Blueprint('main', __name__)
@@ -15,6 +16,37 @@ def fetch_matches():
         )
     return Response(
         json.dumps(matches, ensure_ascii=False, indent=4),  # Formatiran JSON
+        status=200,
+        mimetype='application/json'
+    )
+@main.route('/team-matches/<int:team_id>', methods=['GET'])
+def fetch_team_matches(team_id):
+    matches = get_team_matches(team_id)
+    if "error" in matches:
+        return Response(
+            json.dumps({'message': matches["error"]}, ensure_ascii=False, indent=4),
+            status=500,
+            mimetype='application/json'
+        )
+    return Response(
+        json.dumps(matches, ensure_ascii=False, indent=4),
+        status=200,
+        mimetype='application/json'
+    )
+
+from .utils import get_team_squad
+
+@main.route('/team-squad/<int:team_id>', methods=['GET'])
+def fetch_team_squad(team_id):
+    squad = get_team_squad(team_id)
+    if "error" in squad:
+        return Response(
+            json.dumps({'message': squad["error"]}, ensure_ascii=False, indent=4),
+            status=500,
+            mimetype='application/json'
+        )
+    return Response(
+        json.dumps(squad, ensure_ascii=False, indent=4),
         status=200,
         mimetype='application/json'
     )
