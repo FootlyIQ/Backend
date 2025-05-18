a3c17d153a2995cb1b027d1a4451c41e792e1ac5
DvorsakGal <gal.dvorsak2002@gmail.com>
2025-05-18 18:06:16 +0200

    added endpoints for most common pass clusters and team_id

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import boto3
 import pandas as pd
 import io
 import os
 import json
+import duckdb
+import urllib.parse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,6 +24,15 @@ s3 = boto3.client(
     aws_secret_access_key=creds['aws_secret_access_key'],
     region_name=creds['region_name']
 )
+
+# === Initialize DuckDB Connection Once ===
+con = duckdb.connect()
+con.execute("INSTALL httpfs; LOAD httpfs;")
+con.execute(f"SET s3_region='{creds['region_name']}';")
+# Optional if you need explicit creds (already set in env vars)
+con.execute(f"SET s3_access_key_id='{creds['aws_access_key_id']}';")
+con.execute(f"SET s3_secret_access_key='{creds['aws_secret_access_key']}';")
+
 
 def load_parquet_from_s3(bucket: str, key: str) -> pd.DataFrame:
     """Load a parquet file from S3 and return it as a DataFrame."""
@@ -41,15 +52,85 @@ def test_be():
 def get_pass_clusters():
     """Returns clustered passes from the parquet file on S3."""
     try:
-        df = load_parquet_from_s3("footlyiq-data", "teams.parquet")
+        df = load_parquet_from_s3("footlyiq-data", "gold/pass_clustering/parquet/teams.parquet")
         # Optional: reduce size before sending to frontend
         #limited = df.head(100)  # send only 100 rows for test
         return jsonify(df.to_dict(orient="records"))
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route("/api/get_team_id", methods=['GET'])
+def get_team_id():
+    """Returns team_id from teams dataframe based on the team name."""
+    team_name = request.args.get("team_name")
+    if not team_name:
+        return jsonify({"error": "team_name is required"}), 400
+    
+    try:
+        s3_path = "s3://footlyiq-data/bronze/teams.parquet"
 
+        result = con.execute("""
+            SELECT team_id, name, country
+            FROM read_parquet(?)
+            WHERE name = ?
+        """, [s3_path, team_name]).fetchdf()
 
+        if result.empty:
+            return jsonify({"error": "Team not found"}), 404
+        
+        return jsonify(result.to_dict(orient="records"))
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+def inside_get_team_id(name):
+    """Returns team_id from teams dataframe based on the team name."""
+    try:
+        s3_path = "s3://footlyiq-data/bronze/teams.parquet"
+
+        result = con.execute("""
+            SELECT team_id
+            FROM read_parquet(?)
+            WHERE name = ?
+        """, [s3_path, name]).fetchdf()
+
+        if result.empty:
+            return None
+        
+        return result.iloc[0]["team_id"] # Return the scalar team_id
+    
+    except Exception as e:
+        print(f"Error in inside_get_team_id: {e}")
+        return None
+    
+
+@app.route("/api/passes/most-common", methods=['GET'])
+def get_most_common_pass_clusters():
+    #POMEBNO: funkcija vraca 0-based cluster labels (0-59), torej pri vizualizaciji moreš offsetat za 1 kot pri MOS-u (cluster+1) !!!!!!!!
+    """Returns clustered passes from the parquet file on S3."""
+    team_name = request.args.get("team_name")
+    print(team_name)
+    if not team_name:
+        return jsonify({"error": "team_name is required"}), 400
+    
+    try:
+        df = load_parquet_from_s3("footlyiq-data", "gold/pass_clustering/parquet/ALL_clustered_passes_1_colab.parquet")
+        team_id = inside_get_team_id(team_name)
+
+        if team_id is None:
+            return jsonify({"error": "Team not found"}), 404
+        
+        print(f"team_id: {team_id}")
+
+        df_passes = df[df["team_id"] == team_id]
+        top_6 = df_passes["label"].value_counts().head(6).index
+        top_6_clusters = df_passes[df_passes["label"].isin(top_6)]
+
+        df_limited = top_6_clusters.head(50)
+        
+        return jsonify(df_limited.to_dict(orient="records"))
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run()
